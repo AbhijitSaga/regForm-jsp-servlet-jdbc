498e1edcfacfb0452b2489d5637e513227b57143
ABHIJIT SAGAR <72645670+AbhijitSaga@users.noreply.github.com>
2021-02-01 02:48:36 -0800

    Create EmployeeServlet.java
    package name-> com.reg.pro.controller
                                   |---->EmployeeServlet.java

---
+++ EmployeeServlet.java
@@ -0,0 +1,69 @@
+package com.reg.pro.controller;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.reg.pro.dao.EmployeeDao;
+import com.reg.pro.model.Employee;
+
+/**
+ * Servlet implementation class EmployeeServlet
+ */
+@WebServlet("/register")
+public class EmployeeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private EmployeeDao employeeDao=new EmployeeDao();  
+	
+	
+	
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EmployeeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		RequestDispatcher dispatcher=request.getRequestDispatcher("/WEB-INF/views/index.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String firstName=request.getParameter("firstName");
+		String lastName=request.getParameter("lastName");
+		String address=request.getParameter("address");
+		String contact=request.getParameter("contact");
+		Employee employee=new Employee();
+		employee.setFirstName(firstName);
+		employee.setLastName(lastName);
+		employee.setAddress(address);
+		employee.setContact(contact);
+		
+		try {
+			employeeDao.registerEmployee(employee);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		RequestDispatcher dispatcher=request.getRequestDispatcher("/WEB-INF/views/employeedetails.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
