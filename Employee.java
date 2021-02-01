50d6c1fe3ff81f5a7ccc729c3c6a5bccc0a08032
ABHIJIT SAGAR <72645670+AbhijitSaga@users.noreply.github.com>
2021-02-01 02:57:56 -0800

    Create Employee.java
    package name-->com.reg.pro.model
                                             |---->Employee

---
+++ Employee.java
@@ -0,0 +1,34 @@
+package com.reg.pro.model;
+
+public class Employee {
+private String firstName;
+private String lastName;
+private String address;
+private String contact;
+public String getFirstName() {
+	return firstName;
+}
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+public String getLastName() {
+	return lastName;
+}
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+public String getContact() {
+	return contact;
+}
+public void setContact(String contact) {
+	this.contact = contact;
+}
+
+
+}
